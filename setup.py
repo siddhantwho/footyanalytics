3db417509a5de0c9eac16f5b952e279b8cc09cfb
siddhantwho <71570870+siddhantwho@users.noreply.github.com>
2020-09-21 18:53:32 -0400

    create setup.py

---
+++ setup.py
@@ -0,0 +1,22 @@
+
+from setuptools import setup, find_packages
+
+setup(
+    name = 'footyanalytics',
+    version = '0.0.1',
+    description = 'Python tools to process and visualize soccer event data',
+    author = 'Siddhant Rao',
+    packages = find_packages('src'),
+    package_dir = {'': 'src'},
+    author_email = 'sr692@cornell.edu',
+    install_requires = [
+        'requests==2.21.0',
+        'pandas==0.25.0',
+        'numpy==1.15.40',
+        'sklearn==0.20.1',
+        'matplotlib==3.0.2',
+        'xgboost==1.1.1',
+        'tqdm=4.28.1'
+    ]
+    )
+    
